63754f7cb069fc9f69c1718b52a8d9eb510dca2a
Swapna765 <sahooswapnarani55@gmail.com>
2026-09-21 12:10:22 +0530

    first commit

---
+++ fileimport/demo.js
@@ -0,0 +1,15 @@
+console.log("Swapna rani sahoo")
+
+let a = {
+    name : "Swapna",
+    age : 22
+}
+
+let sum = (a, b) =>{
+    console.log(a+b)
+}
+
+module.exports = {
+    a,
+    sum
+}
---
+++ fileimport/server.js
@@ -0,0 +1,9 @@
+console.log("Swapna")
+
+const xyz = require('./demo.js')
+
+let asd = xyz.a;
+console.log(asd)
+console.log(xyz.a.age)
+
+xyz.sum(2,4)
